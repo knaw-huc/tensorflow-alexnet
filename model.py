a674bba4c6f4cfad196efdffa868091807fddd84
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2020-05-06 11:07:01 +0200

    Code cleanup

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ n_classes = 10 # number of classes in cifar set
 
 image_size = 32
 
-dropout = tf.placeholder(tf.float32, name="dropout_rate")
-input_images = tf.placeholder(tf.float32, shape=[None, image_size, image_size, 3], name="input_images")
+dropout = tf.compat.v1.placeholder(tf.float32, name="dropout_rate")
+input_images = tf.compat.v1.placeholder(tf.float32, shape=[None, image_size, image_size, 3], name="input_images")
 
 ## First convolutional layer
-kernel = tf.Variable(tf.truncated_normal([11,11,3,96], dtype=tf.float32,stddev=1e-1), name="conv1_weights")
+kernel = tf.Variable(tf.random.truncated_normal([11,11,3,96], dtype=tf.float32,stddev=1e-1), name="conv1_weights")
 conv = tf.nn.conv2d(input_images, kernel, [1,4,4,1], padding="SAME")
 bias = tf.Variable(tf.truncated_normal([96]))
 conv_with_bias = tf.nn.bias_add(conv, bias)
@@ -18,7 +18,7 @@ conv1 = tf.nn.relu(conv_with_bias, name="conv1")
 # local response normalization see: https://prateekvjoshi.com/2016/04/05/what-is-local-response-normalization-in-convolutional-neural-networks/
 lrn1 = tf.nn.lrn(conv1, alpha=1e-4, beta=0.75, depth_radius=2, bias=2.0)
 
-pooled_conv1 = tf.nn.max_pool(lrn1, ksize=[1,3,3,1], strides=[1,2,2,1], padding="SAME", name="pool1")
+pooled_conv1 = tf.nn.max_pool2d(lrn1, ksize=[1,3,3,1], strides=[1,2,2,1], padding="SAME", name="pool1")
 
 ## Second convolutional layer
 kernel = tf.Variable(tf.truncated_normal([5, 5, 96, 256],
@@ -63,14 +63,14 @@ weights = tf.Variable(tf.truncated_normal([fc_size, fc_size]), name="fc1_weights
 bias = tf.Variable(tf.truncated_normal([fc_size]), name="fc1_bias")
 fc1 = tf.matmul(conv5, weights) + bias
 fc1 = tf.nn.relu(fc1, name="fc1")
-fc1 = tf.nn.dropout(fc1, dropout)
+fc1 = tf.nn.dropout(fc1, rate = (1 - dropout))
 
 # Second fully connected layer
 weights = tf.Variable(tf.truncated_normal([fc_size, fc_size]), name="fc2_weights")
 bias = tf.Variable(tf.truncated_normal([fc_size]), name="fc2_bias")
 fc2 = tf.matmul(fc1, weights) + bias
 fc2 = tf.nn.relu(fc2, name="fc2")
-fc2 = tf.nn.dropout(fc2, dropout)
+fc2 = tf.nn.dropout(fc2, rate = (1 - dropout))
 
 # Output layer
 weights = tf.Variable(tf.zeros([fc_size, n_classes]), name="output_weight")
--- train.py
+++ train.py
@@ -9,28 +9,27 @@ batch_size = 16
 no_of_epochs = 10
 dropout_rate = 0.8
 
-y = tf.placeholder(tf.float32, [None, model.n_classes])
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model.out, labels=y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+y = tf.compat.v1.placeholder(tf.float32, [None, model.n_classes])
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model.out, labels=y))
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 correct_pred = tf.equal(tf.argmax(model.out, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-tf.summary.histogram("cost", cost)          # <- Added this summary of cost
-tf.summary.histogram("accuracy", accuracy)  # <- Added this summary of accuracy
+tf.compat.v1.summary.histogram("cost", cost)          # <- Added this summary of cost
+tf.compat.v1.summary.histogram("accuracy", accuracy)  # <- Added this summary of accuracy
 
 cifar = Cifar(batch_size=batch_size)
 
 init = tf.initialize_all_variables()
 
-saver = tf.train.Saver()
+saver = tf.compat.v1.train.Saver()
 i = 0
 
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
     sess.run(init)
     
-    # Create reader and writer, which writes to ./logs folder
-    reader = tf.WholeFileReader()
-    writer = tf.summary.FileWriter( './logs/', sess.graph)
+    # Create writer, which writes to ./logs folder  
+    writer = tf.compat.v1.summary.FileWriter( './logs/', sess.graph)
 
     for epoch in range(no_of_epochs):
         for batch in tqdm(cifar.batches, desc="Epoch {}".format(epoch), unit="batch"):
@@ -39,7 +38,7 @@ with tf.Session() as sess:
             sess.run([optimizer], feed_dict={model.input_images: inp, y: out, model.dropout: dropout_rate})
 
 
-        merge = tf.summary.merge_all()
+        merge = tf.compat.v1.summary.merge_all()
         acc, loss, summary = sess.run([accuracy, cost, merge], feed_dict={model.input_images: inp, y: out, model.dropout: 1.})
         writer.add_summary(summary, i)
         i+=1
